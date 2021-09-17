a9b99841142e48a3cf4f8cd47c0d1b0ea159a0c1
nimeshaTJ <nimeshatj@gmail.com>
2021-09-17 22:04:02 +0530

    Cats

---
+++ Cats.py
@@ -0,0 +1,14 @@
+#
+# Author : Nimesha Jayatunge
+# ID : 20464631
+#
+# Cats.py - Simulation of cats in an environment
+#
+# Revisions: 
+#
+# 
+#
+
+import random
+import matplotlib.pyplot as plt
+import numpy as np
